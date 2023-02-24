f85f729cc4791f8204d46b46bdb4f27e38298ed8
Lokesh Dewanand Choudhary <lokesh_choudhary@MacBook-Air.local>
2023-02-24 12:12:14 +0530

    Deploying dummy

--- src/pages/hospitals/index.js
+++ src/pages/hospitals/index.js
@@ -1,22 +1,28 @@
-import HospitalContainer from "@/components/Hospitals/HospitalsContainer";
-import { getAllHospitals } from "@/Utils/fetching";
+// import HospitalContainer from "@/components/Hospitals/HospitalsContainer";
+// import { getAllHospitals } from "@/Utils/fetching";
 
-export default function hospital({ data }) {
-	return (
-		<div>
-			{/* <HospitalContainer hospitals={data} /> */}
+// export default function hospital({ data }) {
+// 	return (
+// 		<div>
+// 			{/* <HospitalContainer hospitals={data} /> */}
 
-			Hospitalssss
-		</div>
-	);
-}
+// 			Hospitalssss
+// 		</div>
+// 	);
+// }
 
-export async function getStaticProps() {
-	const response = await getAllHospitals();
+// export async function getStaticProps() {
+// 	const response = await getAllHospitals();
 
-	return {
-		props: {
-			data: response.results,
-		},
-	};
+// 	return {
+// 		props: {
+// 			data: response.results,
+// 		},
+// 	};
+// }
+
+
+
+export default function allHospitals() {
+	return <div>Hospitals Cards</div>;
 }
