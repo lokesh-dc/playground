bd471e4327ee27a221eee03605a558a581332bfc
Lokesh Dewanand Choudhary <lokesh_choudhary@MacBook-Air.local>
2023-02-24 11:53:10 +0530

    Checking

--- src/Utils/fetching.js
+++ src/Utils/fetching.js
@@ -1,6 +1,7 @@
 export async function getAllDoctors(page, size) {
 	console.log(
-		`${process.env.NEXT_PUBLIC_API_BASELINK}/api/doctors/all?page=${page}&limit=${size}`
+		"environment variable => ",
+		`${process.env.NEXT_PUBLIC_API_BASELINK}`
 	);
 	const res = await fetch(
 		`${process.env.NEXT_PUBLIC_API_BASELINK}/api/doctors/all?page=${page}&limit=${size}`
@@ -8,15 +9,23 @@ export async function getAllDoctors(page, size) {
 		.then((res) => res.json())
 		.then((res) => res)
 		.catch((e) => console.log(e));
+
+	console.log("Doctors Results : ", response);
+
 	return res;
 }
 
 export async function getAllHospitals() {
+	console.log(
+		"environment variable => ",
+		`${process.env.NEXT_PUBLIC_API_BASELINK}`
+	);
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_API_BASELINK}/api/hospitals/all`
 	)
 		.then((res) => res.json())
 		.then((res) => res);
 
+	console.log("Hospital Results : ", response);
 	return response;
 }
