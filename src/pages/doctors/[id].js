01089f6dd1cf9b8eaf05c492aefbc2232438219c
Lokesh Dewanand Choudhary <lokesh_choudhary@MacBook-Air.local>
2023-02-24 12:17:32 +0530

    DEploying website

--- src/pages/doctors/[id].js
+++ src/pages/doctors/[id].js
@@ -1,85 +1,85 @@
-// import { getAllDoctors } from "@/Utils/fetching";
-// import Head from "next/head";
-// import styles from "../../styles/SpecificDoctor.module.css";
+import { getAllDoctors } from "@/Utils/fetching";
+import Head from "next/head";
+import styles from "../../styles/SpecificDoctor.module.css";
 
-// export default function specificDoctorDetails({ data }) {
-// 	return (
-// 		<>
-// 			<Head>
-// 				<title>{data?.DoctorName}</title>
-// 			</Head>
-// 			<div className={styles.specificDoctor}>
-// 				<div>
-// 					<h1>{data?.DoctorName}</h1>
-// 					<h3>{data?.Specialization}</h3>
-// 					<p>Experience : {data?.experience_years} years</p>
+export default function specificDoctorDetails({ data }) {
+	return (
+		<>
+			<Head>
+				<title>{data?.DoctorName}</title>
+			</Head>
+			<div className={styles.specificDoctor}>
+				<div>
+					<h1>{data?.DoctorName}</h1>
+					<h3>{data?.Specialization}</h3>
+					<p>Experience : {data?.experience_years} years</p>
 
-// 					<div dangerouslySetInnerHTML={{ __html: data?.IntroText }} />
-// 					<div className={styles.userFeedbacks}>
-// 						<div>
-// 							<img src="/icons/love.png" />
-// 							97%
-// 						</div>
-// 						<div>
-// 							<img src="/icons/review.png" />
-// 							55 Reviewers
-// 						</div>
-// 					</div>
-// 					<p className={styles.enquiry}>
-// 						38 Patients enquired about the Doctor in last 1 hour
-// 					</p>
-// 					<div>
-// 						<button className={styles.book}>Book Appointment</button>
-// 						<button className={styles.whatsAppButton}>WhatsApp Expert</button>
-// 					</div>
-// 				</div>
-// 				<img src="https://www.hexahealth.com/images/doctor.png" />
-// 			</div>
-// 		</>
-// 	);
-// }
-
-// export async function getStaticPaths() {
-// 	// const data = await getAllDoctors(0, 9950);
-// 	// const paths = data?.results.map((doc) => {
-// 	// 	return {
-// 	// 		params: {
-// 	// 			id: `${doc.Slug_1}`,
-// 	// 		},
-// 	// 	};
-// 	// });
-// 	return {
-// 		paths: [{ params: { id: "dr-alok-chopra" } }],
-// 		fallback: true,
-// 	};
-// }
-
-// export async function getStaticProps(req) {
-// 	let { id } = req.params;
-// 	let data = await fetch(
-// 		`${process.env.NEXT_PUBLIC_API_BASELINK}/api/doctors/details`,
-// 		{
-// 			method: "POST",
-// 			body: JSON.stringify({ slug: id }),
-// 		}
-// 	)
-// 		.then((res) => {
-// 			return res.json();
-// 		})
-// 		.then((res) => res)
-// 		.catch(() => null);
-
-// 	if (data.notFound)
-// 		return {
-// 			notFound: true,
-// 		};
-// 	return {
-// 		props: {
-// 			data: data.results[0],
-// 		},
-// 	};
-// }
-
-export default function specificDoctorDetails() {
-	return <div>Specific Doctor Details</div>;
+					<div dangerouslySetInnerHTML={{ __html: data?.IntroText }} />
+					<div className={styles.userFeedbacks}>
+						<div>
+							<img src="/icons/love.png" />
+							97%
+						</div>
+						<div>
+							<img src="/icons/review.png" />
+							55 Reviewers
+						</div>
+					</div>
+					<p className={styles.enquiry}>
+						38 Patients enquired about the Doctor in last 1 hour
+					</p>
+					<div>
+						<button className={styles.book}>Book Appointment</button>
+						<button className={styles.whatsAppButton}>WhatsApp Expert</button>
+					</div>
+				</div>
+				<img src="https://www.hexahealth.com/images/doctor.png" />
+			</div>
+		</>
+	);
 }
+
+export async function getStaticPaths() {
+	const data = await getAllDoctors(0, 9950);
+	const paths = data?.results.map((doc) => {
+		return {
+			params: {
+				id: `${doc.Slug_1}`,
+			},
+		};
+	});
+	return {
+		paths,
+		fallback: true,
+	};
+}
+
+export async function getStaticProps(req) {
+	let { id } = req.params;
+	let data = await fetch(
+		`${process.env.NEXT_PUBLIC_API_BASELINK}/api/doctors/details`,
+		{
+			method: "POST",
+			body: JSON.stringify({ slug: id }),
+		}
+	)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => res)
+		.catch(() => null);
+
+	if (data.notFound)
+		return {
+			notFound: true,
+		};
+	return {
+		props: {
+			data: data.results[0],
+		},
+	};
+}
+
+// export default function specificDoctorDetails() {
+// 	return <div>Specific Doctor Details</div>;
+// }
--- src/pages/hospitals/index.js
+++ src/pages/hospitals/index.js
@@ -1,28 +1,28 @@
-// import HospitalContainer from "@/components/Hospitals/HospitalsContainer";
-// import { getAllHospitals } from "@/Utils/fetching";
+import HospitalContainer from "@/components/Hospitals/HospitalsContainer";
+import { getAllHospitals } from "@/Utils/fetching";
 
-// export default function hospital({ data }) {
-// 	return (
-// 		<div>
-// 			{/* <HospitalContainer hospitals={data} /> */}
+export default function hospital({ data }) {
+	return (
+		<div>
+			{/* <HospitalContainer hospitals={data} /> */}
 
-// 			Hospitalssss
-// 		</div>
-// 	);
-// }
-
-// export async function getStaticProps() {
-// 	const response = await getAllHospitals();
-
-// 	return {
-// 		props: {
-// 			data: response.results,
-// 		},
-// 	};
-// }
-
-
-
-export default function allHospitals() {
-	return <div>Hospitals Cards</div>;
+			Hospitalssss
+		</div>
+	);
 }
+
+export async function getStaticProps() {
+	const response = await getAllHospitals();
+
+	return {
+		props: {
+			data: response.results,
+		},
+	};
+}
+
+
+
+// export default function allHospitals() {
+// 	return <div>Hospitals Cards</div>;
+// }
